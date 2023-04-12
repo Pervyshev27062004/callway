39027494236865d0c296f9a1a1d68a2f50aefcda
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-04-12 21:35:54 +0300

    SOMETHING IN LESSON AT HOME

--- callway/settings.py
+++ callway/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'callway.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "todo",
+       "USER": "todo",
+       "PASSWORD": "todo",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
+
 
 
 # Password validation
