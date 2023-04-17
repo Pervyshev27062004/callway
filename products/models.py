d0b5aec0c7809fea4ccfc77d9537e460e30d22e9
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-04-17 15:58:07 +0300

    SOMETHING IN LESSON AT HOME1

---
+++ products/models.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.db import models
+from decimal import Decimal
+
+COLOR_CHOICES = (
+    ("RED", "Red"),
+    ("GREEN", "Green"),
+    ("BLUE", "Blue"),
+)
+
+
+class Product(models.Model):
+    external_id = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    color = models.CharField(max_length=32, choices=COLOR_CHOICES, default="RED")
+    price = models.DecimalField(default=Decimal("0"), decimal_places=5, max_digits=10)
+    excerpt = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
+
+    def __str__(self):
+        return f"Product: {self.title} - {self.price} "
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="purchases"
+    )
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="purchases"
+    )
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="addresses"
+    )
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
